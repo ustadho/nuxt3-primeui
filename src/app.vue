0ba01bb28d8aad2f25a70d663463fca88381b75e
ustadho <cak.ust@gmail.com>
2022-11-30 17:35:06 +0700

    Initial commit

---
+++ src/app.vue
@@ -0,0 +1,10 @@
+<script lang="ts" setup>
+const ready = useTimeout(500);
+</script>
+
+<template>
+  <NuxtLayout v-show="ready">
+    <NuxtLoadingIndicator />
+    <NuxtPage />
+  </NuxtLayout>
+</template>
